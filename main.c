de92fbd38182c084b52ea17f5fa0decda298cb18
Supanat​ Khanthong <supanatkhanthong@gmail.com>
2020-10-30 10:48:06 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main(void) {
+ char n[100];
+ int temper;
+ printf("Enter your name : ");
+ scanf("%s",n);
+ printf("Hello %s",n);
+ //int temp[7][2]=
+ 
+ 
+ int Max, Min;
+ return 0;
+}
